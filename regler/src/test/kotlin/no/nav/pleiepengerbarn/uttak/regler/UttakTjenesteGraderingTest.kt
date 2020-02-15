82a6dfb462067da14a1b902624c5d28111c35a16
Frode Lindås <frodeli@gmail.com>
2020-02-15 21:31:54 +0100

    GradBeregning oppdatert.

--- regler/src/test/kotlin/no/nav/pleiepengerbarn/uttak/regler/UttakTjenesteGraderingTest.kt
+++ regler/src/test/kotlin/no/nav/pleiepengerbarn/uttak/regler/UttakTjenesteGraderingTest.kt
@@ -6,14 +6,19 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.time.Month
+import java.util.*
 
 internal class UttakTjenesteGraderingTest {
 
-    private val arbeidsforhold1 = Arbeidsforhold(arbeidstype = Arbeidstype.ARBEIDSGIVER, organisasjonsnummer = "123456789")
+    private val arbeidsforhold1 = Arbeidsforhold(arbeidstype = Arbeidstype.ARBEIDSGIVER, organisasjonsnummer = "123456789", arbeidsforholdId = UUID.randomUUID())
+    private val arbeidsforhold2 = Arbeidsforhold(arbeidstype = Arbeidstype.ARBEIDSGIVER, organisasjonsnummer = "123456789", arbeidsforholdId = UUID.randomUUID())
+    private val arbeidsforhold3 = Arbeidsforhold(arbeidstype = Arbeidstype.ARBEIDSGIVER, organisasjonsnummer = "987654321", arbeidsforholdId = UUID.randomUUID())
+    private val arbeidsforhold4 = Arbeidsforhold(arbeidstype = Arbeidstype.ARBEIDSGIVER, organisasjonsnummer = "987654321", arbeidsforholdId = UUID.randomUUID())
+
+    private val helePerioden = LukketPeriode(LocalDate.of(2020, Month.JANUARY, 1), LocalDate.of(2020, Month.JANUARY, 31))
 
     @Test
     fun `En uttaksperiode med overlappende tilsynsperiode skal føre til redusert grad på uttaksperiode`() {
-        val helePerioden = LukketPeriode(LocalDate.of(2020, Month.JANUARY, 1), LocalDate.of(2020, Month.JANUARY, 31))
         val grunnlag = RegelGrunnlag(
                 tilsynsbehov = listOf(
                         Tilsynsbehov(helePerioden, TilsynsbehovStørrelse.PROSENT_100)
@@ -34,7 +39,6 @@ internal class UttakTjenesteGraderingTest {
 
     @Test
     fun `En uttaksperiode med overlappende arbeidsperiode skal føre til redusert grad på uttaksperiode`() {
-        val helePerioden = LukketPeriode(LocalDate.of(2020, Month.JANUARY, 1), LocalDate.of(2020, Month.JANUARY, 31))
         val grunnlag = RegelGrunnlag(
                 tilsynsbehov = listOf(
                         Tilsynsbehov(helePerioden, TilsynsbehovStørrelse.PROSENT_100)
@@ -61,8 +65,7 @@ internal class UttakTjenesteGraderingTest {
     }
 
     @Test
-    fun `En uttaksperiode med overlappende arbeidsperiode og overlappende tilsyn så skal tilsynsgrad overstyre arbeidsgradgrad dersom tilsynsgrad er større enn arbeidsgrad`() {
-        val helePerioden = LukketPeriode(LocalDate.of(2020, Month.JANUARY, 1), LocalDate.of(2020, Month.JANUARY, 31))
+    fun `En uttaksperiode med overlappende arbeidsperiode og uttak på annen part skal føre til redusert grad på uttaksperiode`() {
         val grunnlag = RegelGrunnlag(
                 tilsynsbehov = listOf(
                         Tilsynsbehov(helePerioden, TilsynsbehovStørrelse.PROSENT_100)
@@ -70,6 +73,11 @@ internal class UttakTjenesteGraderingTest {
                 søktePerioder = listOf(
                         helePerioden
                 ),
+                andrePartersUttaksplan = listOf(Uttaksplan(perioder = listOf(
+                    Uttaksperiode(periode = helePerioden, uttaksperiodeResultat = UttaksperiodeResultat(
+                            grad = Prosent(40)
+                    ))
+                ))),
                 arbeidsforhold = mapOf(
                         arbeidsforhold1 to listOf(
                                 Arbeid(
@@ -79,22 +87,49 @@ internal class UttakTjenesteGraderingTest {
                                         inntekt = Beløp(1000)
                                 )
                         )
-                ),
-                tilsynPerioder = listOf(
-                        Tilsyn(periode = helePerioden, grad = Prosent(30))
                 )
         )
 
         val uttaksplan = UttakTjeneste.uttaksplan(grunnlag)
 
         assertTrue(uttaksplan.perioder.size == 1)
-        sjekkInnvilget(uttaksplan.perioder[0], helePerioden.copy(tom = LocalDate.of(2020, Month.JANUARY, 31)), Prosent(70))
+        sjekkInnvilget(uttaksplan.perioder[0], helePerioden.copy(tom = LocalDate.of(2020, Month.JANUARY, 31)), Prosent(60))
     }
 
 
     @Test
-    fun `En uttaksperiode med gradering mot arbeids for overlappende arbeidsperiode og overlappende tilsyn så skal tilsynsgrad overstyre arbeidsgradgrad dersom tilsynsgrad er større enn arbeidsgrad`() {
-        val helePerioden = LukketPeriode(LocalDate.of(2020, Month.JANUARY, 1), LocalDate.of(2020, Month.JANUARY, 31))
+    fun `En uttaksperiode med mer arbeid enn tilsyn, så skal perioden graderes mot arbeid`() {
+        val grunnlag = RegelGrunnlag(
+                tilsynsbehov = listOf(
+                        Tilsynsbehov(helePerioden, TilsynsbehovStørrelse.PROSENT_100)
+                ),
+                søktePerioder = listOf(
+                        helePerioden
+                ),
+                arbeidsforhold = mapOf(
+                        arbeidsforhold1 to listOf(
+                                Arbeid(
+                                        arbeidsforhold = arbeidsforhold1,
+                                        periode = helePerioden,
+                                        arbeidsprosent = Prosent(35),
+                                        inntekt = Beløp(1000)
+                                )
+                        )
+                ),
+                tilsynPerioder = listOf(
+                        Tilsyn(periode = helePerioden, grad = Prosent(30))
+                )
+        )
+
+        val uttaksplan = UttakTjeneste.uttaksplan(grunnlag)
+
+        assertTrue(uttaksplan.perioder.size == 1)
+        sjekkInnvilget(uttaksplan.perioder[0], helePerioden.copy(tom = LocalDate.of(2020, Month.JANUARY, 31)), Prosent(65))
+    }
+
+
+    @Test
+    fun `En uttaksperiode med mer tilsyn enn arbeid, så skal perioden graderes mot arbeid`() {
         val grunnlag = RegelGrunnlag(
                 tilsynsbehov = listOf(
                         Tilsynsbehov(helePerioden, TilsynsbehovStørrelse.PROSENT_100)
@@ -123,7 +158,59 @@ internal class UttakTjenesteGraderingTest {
         sjekkInnvilget(uttaksplan.perioder[0], helePerioden.copy(tom = LocalDate.of(2020, Month.JANUARY, 31)), Prosent(70))
     }
 
+    @Test
+    fun `En uttaksperioder med fire arbeidsforhold som skal vurderes til gradering mot tilsyn`() {
+        val grunnlag = RegelGrunnlag(
+                tilsynsbehov = listOf(
+                        Tilsynsbehov(helePerioden, TilsynsbehovStørrelse.PROSENT_100)
+                ),
+                søktePerioder = listOf(
+                        helePerioden
+                ),
+                arbeidsforhold = mapOf(
+                        arbeidsforhold1 to listOf(
+                                Arbeid(
+                                        arbeidsforhold = arbeidsforhold1,
+                                        periode = helePerioden,
+                                        arbeidsprosent = Prosent(40),
+                                        inntekt = Beløp(2000)
+                                )
+                        ),
+                        arbeidsforhold2 to listOf(
+                                Arbeid(
+                                        arbeidsforhold = arbeidsforhold2,
+                                        periode = helePerioden,
+                                        arbeidsprosent = Prosent(20),
+                                        inntekt = Beløp(1500)
+                                )
+                        ),
+                        arbeidsforhold3 to listOf(
+                                Arbeid(
+                                        arbeidsforhold = arbeidsforhold3,
+                                        periode = helePerioden,
+                                        arbeidsprosent = Prosent(80),
+                                        inntekt = Beløp(500)
+                                )
+                        ),
+                        arbeidsforhold4 to listOf(
+                                Arbeid(
+                                        arbeidsforhold = arbeidsforhold4,
+                                        periode = helePerioden,
+                                        arbeidsprosent = Prosent(0),
+                                        inntekt = Beløp(1000)
+                                )
+                        )
+                ),
+                tilsynPerioder = listOf(
+                        Tilsyn(periode = helePerioden, grad = Prosent(40))
+                )
+        )
 
+        val uttaksplan = UttakTjeneste.uttaksplan(grunnlag)
+
+        sjekkInnvilget(uttaksplan.perioder[0], helePerioden, Prosent(60))
+
+    }
 
 
 }
