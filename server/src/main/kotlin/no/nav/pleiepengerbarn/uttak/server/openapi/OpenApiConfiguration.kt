2b081a5889145decdbf3acb8daba5edd25497747
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-02-23 17:22:07 +0100

    LukketPeriode serialiseres og deserialiseres iht is 8601

--- server/src/main/kotlin/no/nav/pleiepengerbarn/uttak/server/openapi/OpenApiConfiguration.kt
+++ server/src/main/kotlin/no/nav/pleiepengerbarn/uttak/server/openapi/OpenApiConfiguration.kt
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 internal class OpenApiConfiguration {
 
-    @Bean 
+    @Bean
     internal fun modelConverter() = CustomModelConverter()
 
     @Bean
