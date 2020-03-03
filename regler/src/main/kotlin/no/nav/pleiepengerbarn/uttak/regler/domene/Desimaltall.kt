1f0ea2556c5d409bd8f6d4a92032b5b42d40e02a
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-03-03 23:39:17 +0100

    Desimal -> Desimaltall

--- regler/src/main/kotlin/no/nav/pleiepengerbarn/uttak/regler/domene/Desimaltall.kt
+++ regler/src/main/kotlin/no/nav/pleiepengerbarn/uttak/regler/domene/Desimaltall.kt
@@ -4,40 +4,40 @@ import java.math.BigDecimal
 import java.time.Duration
 import java.math.RoundingMode.HALF_UP
 
-internal class Desimal(bigDecimal: BigDecimal) {
+internal class Desimaltall(bigDecimal: BigDecimal) {
     internal companion object {
         private const val Scale = 2
         private val RoundingMode = HALF_UP
 
-        internal fun fraBigDecimal(bigDecimal: BigDecimal) = Desimal(bigDecimal)
-        internal fun fraDouble(double: Double) = Desimal(BigDecimal.valueOf(double))
-        internal fun fraDuration(duration: Duration) = Desimal(BigDecimal.valueOf(duration.toMillis()))
+        internal fun fraBigDecimal(bigDecimal: BigDecimal) = Desimaltall(bigDecimal)
+        internal fun fraDouble(double: Double) = Desimaltall(BigDecimal.valueOf(double))
+        internal fun fraDuration(duration: Duration) = Desimaltall(BigDecimal.valueOf(duration.toMillis()))
 
         internal val En = fraDouble(1.00)
         internal val Null = fraDouble(0.00)
         internal val EtHundre = fraDouble(100.00)
     }
 
-    // Gjør at vi kan stole på bruk av == mellom Desimal
+    // Gjør at vi kan stole på bruk av == mellom Desimaltall
     override fun equals(other: Any?): Boolean {
-        return if (other == null || other !is Desimal) false
+        return if (other == null || other !is Desimaltall) false
         else compareTo(other) == 0
     }
     override fun hashCode() = value?.hashCode() ?: 0
     override fun toString(): String = value.toPlainString()
 
-    // Bruke <=> mellom Desimal
-    internal operator fun compareTo(other: Desimal) = value.compareTo(other.value)
-    // Bruke + mellom Desimal
-    internal operator fun plus(annen: Desimal) = Desimal(value.add(annen.value))
-    // Bruke - mellom Desimal
-    internal operator fun minus(annen: Desimal) = Desimal(value.subtract(annen.value))
-    // Bruke * mellom Desimal
-    internal operator fun times(annen: Desimal) = Desimal(value.multiply(annen.value))
-    // Bruke / mellom Desimal
-    internal operator fun div(annen: Desimal) = Desimal(value.divide(annen.value, Scale, RoundingMode))
+    // Bruke <=> mellom Desimaltall
+    internal operator fun compareTo(other: Desimaltall) = value.compareTo(other.value)
+    // Bruke + mellom Desimaltall
+    internal operator fun plus(annen: Desimaltall) = Desimaltall(value.add(annen.value))
+    // Bruke - mellom Desimaltall
+    internal operator fun minus(annen: Desimaltall) = Desimaltall(value.subtract(annen.value))
+    // Bruke * mellom Desimaltall
+    internal operator fun times(annen: Desimaltall) = Desimaltall(value.multiply(annen.value))
+    // Bruke / mellom Desimaltall
+    internal operator fun div(annen: Desimaltall) = Desimaltall(value.divide(annen.value, Scale, RoundingMode))
 
-    internal infix fun maks(maks: Desimal) = if (this > maks) maks else this
+    internal infix fun maks(maks: Desimaltall) = if (this > maks) maks else this
 
     internal fun fraFaktorTilProsent() = this * EtHundre
     internal fun fraProsentTilFaktor() = this / EtHundre
@@ -56,7 +56,7 @@ internal class Desimal(bigDecimal: BigDecimal) {
 }
 
 // Kunne dele duration på en annen Duration
-internal operator fun Duration.div(annen: Duration) = Desimal.fraDuration(this) / Desimal.fraDuration(annen)
+internal operator fun Duration.div(annen: Duration) = Desimaltall.fraDuration(this) / Desimaltall.fraDuration(annen)
 // Unngå å måtte bruke dividedBy på Duration
 internal operator fun Duration.div(long: Long) = dividedBy(long)
 // Unngå å måtte bruke multipliedBy på Duration
