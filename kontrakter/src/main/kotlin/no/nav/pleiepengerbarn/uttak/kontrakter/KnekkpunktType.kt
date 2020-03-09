685965966860328adeaba3943e1193d003e695a1
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-03-09 20:25:21 +0100

    Endret format på KnekkpunktTyper

--- kontrakter/src/main/kotlin/no/nav/pleiepengerbarn/uttak/kontrakter/KnekkpunktType.kt
+++ kontrakter/src/main/kotlin/no/nav/pleiepengerbarn/uttak/kontrakter/KnekkpunktType.kt
@@ -1,11 +1,11 @@
 package no.nav.pleiepengerbarn.uttak.kontrakter
 
-enum class KnekkpunktType { // TODO: Samme format som andre Enums
-    FERIE,
-    TILSYNSBEHOV,
-    TILSYNSPERIODE,
-    ANNEN_PARTS_UTTAK,
-    ARBEID,
-    IKKE_MEDLEM,
-    SØKERS_DØDSFALL
+enum class KnekkpunktType {
+    Ferie,
+    Tilsynsbehov,
+    Tilsynsperiode,
+    AnnenPartsUttak,
+    Arbeid,
+    IkkeMedlem,
+    SøkersDødsfall
 }
