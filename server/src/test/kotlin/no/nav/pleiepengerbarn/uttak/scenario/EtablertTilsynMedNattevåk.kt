96883745c776dcff3ad9e8ce4855a2c3c91968f5
Frode Lindås <frodeli@gmail.com>
2021-04-13 16:37:41 +0200

    Nytt test scenario.

---
+++ server/src/test/kotlin/no/nav/pleiepengerbarn/uttak/scenario/EtablertTilsynMedNattevåk.kt
@@ -0,0 +1,21 @@
+package no.nav.pleiepengerbarn.uttak.scenario
+
+import no.nav.pleiepengerbarn.uttak.kontrakter.LukketPeriode
+import no.nav.pleiepengerbarn.uttak.kontrakter.Utfall
+import no.nav.pleiepengerbarn.uttak.kontrakter.Vilkårsperiode
+import no.nav.pleiepengerbarn.uttak.testklient.lagGrunnlag
+import no.nav.pleiepengerbarn.uttak.testklient.testClientMotLokalServer
+import java.time.Duration
+
+fun main() {
+    val grunnlag = lagGrunnlag(periode = "2020-01-01/2020-01-10")
+        .copy(
+            inngangsvilkår = mapOf("FP_VK_2" to listOf(Vilkårsperiode(LukketPeriode("2020-01-01/2020-01-10"), Utfall.OPPFYLT))),
+            tilsynsperioder = mapOf(LukketPeriode("2020-01-01/2020-01-10") to Duration.ofHours(5)),
+            nattevåksperioder = setOf(LukketPeriode("2020-01-06/2020-01-10"))
+
+        )
+    testClientMotLokalServer().opprettUttaksplan(grunnlag)
+    println("Opprettet uttaksplan for behandlingUUID = ${grunnlag.behandlingUUID}")
+}
+
