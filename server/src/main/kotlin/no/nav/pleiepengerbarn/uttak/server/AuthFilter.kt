6439ca4c6bb9401176e04ad6168a874e9a0281aa
Frode Lindås <frodeli@gmail.com>
2021-06-01 09:28:08 +0200

    Lagt på enkel sikkhetsmekanisme. (#178)
    * Lagt på enkel sikkhetsmekanisme.
    
    * Denne trengte vi ikke likevel.
    
    * Default verdi på token for tester.

---
+++ server/src/main/kotlin/no/nav/pleiepengerbarn/uttak/server/AuthFilter.kt
@@ -0,0 +1,27 @@
+package no.nav.pleiepengerbarn.uttak.server
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.io.IOException
+import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class AuthFilter : Filter {
+
+    @Value("\${NAV_PSB_UTTAK_TOKEN:no_secret}")
+    private lateinit var sharedSecret
+            : String
+
+    @Throws(IOException::class, ServletException::class)
+    override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
+        val request = req as HttpServletRequest
+        val response = res as HttpServletResponse
+        if (sharedSecret != request.getHeader("Nav-Psb-Uttak-Token")) {
+            response.sendError(403)
+            return
+        }
+        chain.doFilter(request, response)
+    }
+}
