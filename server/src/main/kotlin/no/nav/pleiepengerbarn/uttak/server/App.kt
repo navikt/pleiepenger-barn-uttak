147a163716522ec299bd645348046b434829ad28
Frode Lindås <frodeli@gmail.com>
2021-06-18 08:59:56 +0200

    Lagt på filter som propagerer callid.

--- server/src/main/kotlin/no/nav/pleiepengerbarn/uttak/server/App.kt
+++ server/src/main/kotlin/no/nav/pleiepengerbarn/uttak/server/App.kt
@@ -1,11 +1,25 @@
 package no.nav.pleiepengerbarn.uttak.server
 
+import no.nav.familie.log.filter.LogFilter
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class App
+class App {
+
+    @Bean
+    fun logFilter(): FilterRegistrationBean<LogFilter> {
+        // Registrer CallId MDC propageringsfilter
+        val filterRegistration: FilterRegistrationBean<LogFilter> = FilterRegistrationBean()
+        filterRegistration.filter = LogFilter()
+        filterRegistration.order = 1
+        return filterRegistration
+    }
+
+}
 
 fun main(args: Array<String>) {
     val app = SpringApplicationBuilder(App::class.java)
