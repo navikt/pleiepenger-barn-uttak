179a3fde8d69680d14c962c23d9a37ca5593a5f0
Frode Lindås <frodeli@gmail.com>
2021-08-10 14:02:46 +0200

    Bugfix ifm sammenslåing av perioder.

--- server/src/test/kotlin/no/nav/pleiepengerbarn/uttak/server/UttaksplanExtTest.kt
+++ server/src/test/kotlin/no/nav/pleiepengerbarn/uttak/server/UttaksplanExtTest.kt
@@ -1,5 +1,8 @@
 package no.nav.pleiepengerbarn.uttak.server
 
+import no.nav.pleiepengerbarn.uttak.kontrakter.*
+import no.nav.pleiepengerbarn.uttak.regler.HUNDRE_PROSENT
+import no.nav.pleiepengerbarn.uttak.regler.NULL_PROSENT
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
@@ -14,5 +17,60 @@ internal class UttaksplanExtTest {
         assertThat(bareHelgMellom(LocalDate.parse("2021-08-05"), LocalDate.parse("2021-08-10"))).isFalse
         assertThat(bareHelgMellom(LocalDate.parse("2021-08-07"), LocalDate.parse("2021-08-10"))).isFalse
     }
+
+    @Test
+    internal fun `Tre sammenhengende perioder skal slås sammen`() {
+        val periode1 = LukketPeriode("2021-08-09/2021-08-13")
+        val periode2 = LukketPeriode("2021-08-16/2021-08-20")
+        val periode3 = LukketPeriode("2021-08-23/2021-08-27")
+        val uttaksplan = Uttaksplan(perioder = mapOf(
+            periode1 to dummyUttaksperiodeInfo(),
+            periode2 to dummyUttaksperiodeInfo(),
+            periode3 to dummyUttaksperiodeInfo()
+        ))
+
+        val nyUttaksplan = uttaksplan.slåSammenLikePerioder()
+
+        assertThat(nyUttaksplan.perioder.keys).hasSize(1)
+        assertThat(nyUttaksplan.perioder[LukketPeriode("2021-08-09/2021-08-27")]).isEqualTo(dummyUttaksperiodeInfo())
+    }
+
+    @Test
+    internal fun `Tre ikke sammenhengende perioder skal ikke slås sammen`() {
+        val periode1 = LukketPeriode("2021-08-09/2021-08-12")
+        val periode2 = LukketPeriode("2021-08-16/2021-08-19")
+        val periode3 = LukketPeriode("2021-08-23/2021-08-26")
+        val uttaksplan = Uttaksplan(perioder = mapOf(
+            periode1 to dummyUttaksperiodeInfo(),
+            periode2 to dummyUttaksperiodeInfo(),
+            periode3 to dummyUttaksperiodeInfo()
+        ))
+
+        val nyUttaksplan = uttaksplan.slåSammenLikePerioder()
+
+        assertThat(nyUttaksplan).isEqualTo(uttaksplan)
+    }
+
+    private fun dummyUttaksperiodeInfo() = UttaksperiodeInfo(
+        utfall = Utfall.OPPFYLT,
+        utbetalingsgrader = listOf(),
+        annenPart = AnnenPart.ALENE,
+        beredskap = null,
+        nattevåk = null,
+        graderingMotTilsyn = GraderingMotTilsyn(
+            etablertTilsyn = NULL_PROSENT,
+            overseEtablertTilsynÅrsak = null,
+            andreSøkeresTilsyn = NULL_PROSENT,
+            tilgjengeligForSøker = NULL_PROSENT,
+            andreSøkeresTilsynReberegnet = false
+        ),
+        kildeBehandlingUUID = "123",
+        oppgittTilsyn = null,
+        pleiebehov = Pleiebehov.PROSENT_100.prosent,
+        søkersTapteArbeidstid = null,
+        uttaksgrad = HUNDRE_PROSENT,
+        årsaker = setOf()
+    )
+
 
 }
