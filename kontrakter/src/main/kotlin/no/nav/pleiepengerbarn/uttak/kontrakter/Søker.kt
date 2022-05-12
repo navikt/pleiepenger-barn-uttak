9baf4875f5ecc5b1d21a5f3013be518cf7e707be
Marius Glittum <marius.glittum@nav.no>
2022-05-12 19:56:08 +0200

    Legger til dødsdato på søker

--- kontrakter/src/main/kotlin/no/nav/pleiepengerbarn/uttak/kontrakter/Søker.kt
+++ kontrakter/src/main/kotlin/no/nav/pleiepengerbarn/uttak/kontrakter/Søker.kt
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.LocalDate
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
 data class Søker(
-        @JsonProperty("aktørId") val aktørId: String
+        @JsonProperty("aktørId") val aktørId: String,
+        @JsonProperty("dødsdato") val dødsdato: LocalDate? = null
 )
