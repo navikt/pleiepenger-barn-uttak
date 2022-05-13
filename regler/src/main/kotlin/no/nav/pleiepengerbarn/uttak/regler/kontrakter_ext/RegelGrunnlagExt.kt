3e623c6260a7754f186521549b9773d0e20e6134
Marius Glittum <marius.glittum@nav.no>
2022-05-13 10:15:31 +0200

    Endrer annen part utledning

--- regler/src/main/kotlin/no/nav/pleiepengerbarn/uttak/regler/kontrakter_ext/RegelGrunnlagExt.kt
+++ regler/src/main/kotlin/no/nav/pleiepengerbarn/uttak/regler/kontrakter_ext/RegelGrunnlagExt.kt
@@ -1,11 +1,14 @@
 package no.nav.pleiepengerbarn.uttak.regler.kontrakter_ext
 
-import no.nav.pleiepengerbarn.uttak.kontrakter.*
+import no.nav.pleiepengerbarn.uttak.kontrakter.AnnenPart
+import no.nav.pleiepengerbarn.uttak.kontrakter.LukketPeriode
+import no.nav.pleiepengerbarn.uttak.kontrakter.Utfall
+import no.nav.pleiepengerbarn.uttak.kontrakter.Uttaksplan
 import no.nav.pleiepengerbarn.uttak.regler.domene.RegelGrunnlag
 import java.util.*
 
 fun RegelGrunnlag.annenPart(periode: LukketPeriode): AnnenPart {
-    if (this.andrePartersUttaksplanPerBehandling.overlapper(periode)) {
+    if (this.andrePartersUttaksplanPerBehandling.filter { this.behandlingUUID != it.key }.overlapper(periode)) {
         return AnnenPart.MED_ANDRE
     }
     return AnnenPart.ALENE
