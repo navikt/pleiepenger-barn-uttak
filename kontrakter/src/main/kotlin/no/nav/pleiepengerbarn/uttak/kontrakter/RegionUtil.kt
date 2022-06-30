1be8cb4be58ecc7c263ffd93d3c5c7651d64f73e
Marius Glittum <marius.glittum@nav.no>
2022-06-30 21:33:31 +0200

    Dokumenterer landkoder og legger til sveits

--- kontrakter/src/main/kotlin/no/nav/pleiepengerbarn/uttak/kontrakter/RegionUtil.kt
+++ kontrakter/src/main/kotlin/no/nav/pleiepengerbarn/uttak/kontrakter/RegionUtil.kt
@@ -3,10 +3,40 @@ package no.nav.pleiepengerbarn.uttak.kontrakter
 class RegionUtil {
     fun erIEØS(landkode: String?): Boolean {
         val eøsLand: List<String> = listOf(
-            "ALA", "AUT", "BEL", "BGR", "CYP", "CZE",
-            "DEU", "DNK", "ESP", "EST", "FIN", "FRA", "FRO", "GBR", "GRC", "GRL",
-            "HRV", "HUN", "IRL", "ISL", "ITA", "LIE", "LTU", "LUX", "LVA", "MLT",
-            "NLD", "NOR", "POL", "PRT", "ROU", "SVK", "SVN", "SWE"
+            "AUT", // Østerrike
+            "BEL", // Belgia
+            "BGR", // Bulgaria
+            "CYP", // Kypros
+            "CZE", // Tsjekkia
+            "DEU", // Tyskland
+            "DNK", // Danmark
+            "FRO", // Færøyene - Danmark
+            "GRL", // Grønnland - Danmark
+            "ESP", // Spania
+            "EST", // Estland
+            "FIN", // Finland
+            "ATA", // Åland - Finland
+            "FRA", // Frankrike
+            "GRC", // Hellas
+            "HRV", // Kroatia
+            "HUN", // Ungarn
+            "IRL", // Irland
+            "ISL", // Island
+            "ITA", // Italia
+            "LIE", // Liechtenstein
+            "LTU", // Litauen
+            "LUX", // Luxembourg
+            "LVA", // Latvia
+            "MLT", // Malta
+            "NLD", // Nederland
+            "NOR", // Norge
+            "POL", // Polen
+            "PRT", // Portugal
+            "ROU", // Romania
+            "SVK", // Slovakia
+            "SVN", // Slovenia
+            "CHE", // Sveits (Sveits er ikke EØS-land, men omfattes av reglene for koordinering av trygd)
+            "SWE" // Sverige
         )
 
         return eøsLand.contains(landkode)
