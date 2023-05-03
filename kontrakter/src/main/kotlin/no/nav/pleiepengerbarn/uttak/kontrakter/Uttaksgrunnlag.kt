0857a49645495827e5a94cb5607d388e70971ea1
Helga Nyrud <helga.nyrud@nav.no>
2023-05-03 16:08:57 +0200

    Legger til overstyring av input i grunnlaget fra k9-sak

--- kontrakter/src/main/kotlin/no/nav/pleiepengerbarn/uttak/kontrakter/Uttaksgrunnlag.kt
+++ kontrakter/src/main/kotlin/no/nav/pleiepengerbarn/uttak/kontrakter/Uttaksgrunnlag.kt
@@ -21,6 +21,7 @@ data class Uttaksgrunnlag (
     @JsonProperty("arbeid") val arbeid: List<Arbeid>,
     @JsonProperty("pleiebehov") val pleiebehov: Map<LukketPeriode, Pleiebehov>,
 
+    @JsonProperty("overstyrtInput") val overstyrtInput: Map<LukketPeriode, Duration> = mapOf(),
     @JsonProperty("lovbestemtFerie") val lovbestemtFerie: List<LukketPeriode> = listOf(),
     @JsonProperty("inngangsvilkår") val inngangsvilkår: Map<String, List<Vilkårsperiode>> = mapOf(),
     @JsonProperty("tilsynsperioder") val tilsynsperioder: Map<LukketPeriode, Duration> = mapOf(),
@@ -53,4 +54,12 @@ data class SøktUttak(
 data class UtenlandsoppholdInfo(
     @JsonProperty("utenlandsoppholdÅrsak") val utenlandsoppholdÅrsak: UtenlandsoppholdÅrsak,
     @JsonProperty("landkode") val landkode: String?
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
+data class OverstyrtInput(
+    @JsonProperty("overstyrtUttaksgrad") val overstyrtUttaksgrad: Duration,
+    @JsonProperty("arbeidsforhold") val arbeidsforhold: Arbeidsforhold
 )
