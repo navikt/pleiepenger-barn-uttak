11f802720a7cd0445ade6a4438b3dfab61db474e
albrektsson <emil.albrektsson@nav.no>
2024-01-19 10:57:53 +0100

    Erstattet PAT med token-support, java 21 + dep bumps

--- server/src/main/kotlin/no/nav/pleiepengerbarn/uttak/server/App.kt
+++ server/src/main/kotlin/no/nav/pleiepengerbarn/uttak/server/App.kt
@@ -1,17 +1,14 @@
 package no.nav.pleiepengerbarn.uttak.server
 
-import org.slf4j.LoggerFactory
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer
 import org.springframework.core.io.ClassPathResource
-import java.util.*
 
 
 @SpringBootApplication
@@ -19,17 +16,6 @@ import java.util.*
 @ConfigurationPropertiesScan("no.nav.pleiepengerbarn.uttak")
 @ComponentScan("no.nav.pleiepengerbarn.uttak")
 class App {
-
-    private val logger = LoggerFactory.getLogger(App::class.java)
-
-    @Bean
-    fun logFilter(): FilterRegistrationBean<LogFilter> {
-        // Registrer CallId MDC propageringsfilter
-        val filterRegistration: FilterRegistrationBean<LogFilter> = FilterRegistrationBean()
-        filterRegistration.filter = LogFilter()
-        filterRegistration.order = 1
-        return filterRegistration
-    }
 
     @Bean
     fun gitPropertiesPlaceholderConfigurer(): PropertySourcesPlaceholderConfigurer {
--- server/src/test/kotlin/no/nav/pleiepengerbarn/uttak/testklient/PleiepengerBarnUttakTestClient.kt
+++ server/src/test/kotlin/no/nav/pleiepengerbarn/uttak/testklient/PleiepengerBarnUttakTestClient.kt
@@ -2,46 +2,91 @@ package no.nav.pleiepengerbarn.uttak.testklient
 
 import no.nav.pleiepengerbarn.uttak.kontrakter.*
 import no.nav.pleiepengerbarn.uttak.server.UttakplanApi
+import no.nav.security.mock.oauth2.MockOAuth2Server
+import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
 
-internal class PleiepengerBarnUttakTestClient(private val restTemplate: TestRestTemplate) {
+internal class PleiepengerBarnUttakTestClient(
+    private val restTemplate: TestRestTemplate,
+    private val token: String
+) {
 
     internal fun opprettUttaksplan(grunnlag: Uttaksgrunnlag): ResponseEntity<Uttaksplan> {
-        return restTemplate.exchange(UttakplanApi.UttaksplanPath, HttpMethod.POST, HttpEntity<Any>(grunnlag, headers()), Uttaksplan::class.java)
+        return restTemplate.exchange(
+            UttakplanApi.UttaksplanPath,
+            HttpMethod.POST,
+            HttpEntity<Any>(grunnlag, headers()),
+            Uttaksplan::class.java
+        )
     }
 
     internal fun simulerUttaksplan(grunnlag: Uttaksgrunnlag): ResponseEntity<Simulering> {
-        return restTemplate.exchange(UttakplanApi.UttaksplanSimuleringPath, HttpMethod.POST, HttpEntity<Any>(grunnlag, headers()), Simulering::class.java)
+        return restTemplate.exchange(
+            UttakplanApi.UttaksplanSimuleringPath,
+            HttpMethod.POST,
+            HttpEntity<Any>(grunnlag, headers()),
+            Simulering::class.java
+        )
     }
 
     internal fun simulerUttaksplanLivetsSluttfase(grunnlag: Uttaksgrunnlag): ResponseEntity<SimuleringLivetsSluttfase> {
-        return restTemplate.exchange(UttakplanApi.UttaksplanSimuleringSluttfasePath, HttpMethod.POST, HttpEntity<Any>(grunnlag, headers()), SimuleringLivetsSluttfase::class.java)
+        return restTemplate.exchange(
+            UttakplanApi.UttaksplanSimuleringSluttfasePath,
+            HttpMethod.POST,
+            HttpEntity<Any>(grunnlag, headers()),
+            SimuleringLivetsSluttfase::class.java
+        )
     }
 
     internal fun nedjusterSøkersUttaksgrad(grunnlag: Uttaksgrunnlag): ResponseEntity<Uttaksplan> {
-        return restTemplate.exchange(UttakplanApi.UttaksplanNedjusterSøkersUttaksgradPath, HttpMethod.POST, HttpEntity<Any>(grunnlag, headers()), Uttaksplan::class.java)
+        return restTemplate.exchange(
+            UttakplanApi.UttaksplanNedjusterSøkersUttaksgradPath,
+            HttpMethod.POST,
+            HttpEntity<Any>(grunnlag, headers()),
+            Uttaksplan::class.java
+        )
     }
 
     internal fun hentUttaksplan(behandlingUUID: BehandlingUUID): ResponseEntity<Uttaksplan> {
-        return restTemplate.exchange(UttakplanApi.UttaksplanPath + "?behandlingUUID=$behandlingUUID", HttpMethod.GET, HttpEntity<Any>(headers()), Uttaksplan::class.java)
+        return restTemplate.exchange(
+            UttakplanApi.UttaksplanPath + "?behandlingUUID=$behandlingUUID",
+            HttpMethod.GET,
+            HttpEntity<Any>(headers()),
+            Uttaksplan::class.java
+        )
     }
 
-    internal fun hentUttaksplan(behandlingUUID: BehandlingUUID, slåSammenLikePerioder: Boolean): ResponseEntity<Uttaksplan> {
-        return restTemplate.exchange(UttakplanApi.UttaksplanPath + "?behandlingUUID=$behandlingUUID&slåSammenLikePerioder=$slåSammenLikePerioder", HttpMethod.GET, HttpEntity<Any>(headers()), Uttaksplan::class.java)
+    internal fun hentUttaksplan(
+        behandlingUUID: BehandlingUUID,
+        slåSammenLikePerioder: Boolean
+    ): ResponseEntity<Uttaksplan> {
+        return restTemplate.exchange(
+            UttakplanApi.UttaksplanPath + "?behandlingUUID=$behandlingUUID&slåSammenLikePerioder=$slåSammenLikePerioder",
+            HttpMethod.GET,
+            HttpEntity<Any>(headers()),
+            Uttaksplan::class.java
+        )
     }
 
     internal fun slettUttaksplan(behandlingUUID: BehandlingUUID) {
-        restTemplate.exchange(UttakplanApi.UttaksplanPath + "?behandlingUUID=$behandlingUUID", HttpMethod.DELETE, HttpEntity<Any>(headers()), Unit::class.java)
+        restTemplate.exchange(
+            UttakplanApi.UttaksplanPath + "?behandlingUUID=$behandlingUUID",
+            HttpMethod.DELETE,
+            HttpEntity<Any>(headers()),
+            Unit::class.java
+        )
     }
 
-    private fun headers():HttpHeaders {
+    private fun headers(): HttpHeaders {
         val headers = HttpHeaders()
-        headers["NAV_PSB_UTTAK_TOKEN"] = listOf("no_secret")
+        headers["Authorization"] = "Bearer $token"
         return headers
     }
 
@@ -50,5 +95,5 @@ internal class PleiepengerBarnUttakTestClient(private val restTemplate: TestRest
 internal fun testClientMotLokalServer(): PleiepengerBarnUttakTestClient {
     val restTemplateBuilder = RestTemplateBuilder()
         .rootUri("http://localhost:8080/pleiepenger-barn-uttak/")
-    return PleiepengerBarnUttakTestClient(TestRestTemplate(restTemplateBuilder))
+    return PleiepengerBarnUttakTestClient(TestRestTemplate(restTemplateBuilder), "token")
 }
