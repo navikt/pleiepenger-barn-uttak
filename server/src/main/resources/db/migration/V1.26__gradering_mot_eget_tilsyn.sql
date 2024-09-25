df2de56914f49201487122b3466afed004059364
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-09-25 15:28:03 +0200

    Fikser db-skript

--- server/src/main/resources/db/migration/V1.26__gradering_mot_eget_tilsyn.sql
+++ server/src/main/resources/db/migration/V1.26__gradering_mot_eget_tilsyn.sql
@@ -1,2 +1,1 @@
-ALTER TABLE uttaksperiode ADD COLUMN uttaksgradMedReduksjonGrunnetInntektsgradering decimal(5,2);
-ALTER TABLE uttaksperiode ADD COLUMN uttaksgradUtenReduksjonGrunnetInntektsgradering decimal(5,2);
+ALTER TABLE uttaksperiode ADD COLUMN graderingMotEgetTilsyn decimal(5,2);
