1e4e4df43beaa6c38af79ef3958f0b2178de9f50
Inga <inga.kristine.holen@nav.no>
2025-03-25 13:38:11 +0100

    TSFF-1101: Vekter ikke-yrkesaktive likt som aktive i beregning av utbetalingsgrad (#1087)

---
+++ regler/src/main/kotlin/no/nav/pleiepengerbarn/uttak/regler/Arbeidstype.kt
@@ -0,0 +1,71 @@
+package no.nav.pleiepengerbarn.uttak.regler
+
+enum class Arbeidstype(val kode: String) {
+    ARBEIDSTAKER("AT"),
+    FRILANSER("FL"),
+    DAGPENGER("DP"),
+    SELVSTENDIG_NÆRINGSDRIVENDE("SN"),
+    IKKE_YRKESAKTIV("IKKE_YRKESAKTIV"),
+    IKKE_YRKESAKTIV_UTEN_ERSTATNING("IKKE_YRKESAKTIV_UTEN_ERSTATNING"),
+    KUN_YTELSE("BA"),
+    INAKTIV("MIDL_INAKTIV"),
+    SYKEPENGER_AV_DAGPENGER("SP_AV_DP"),
+    PSB_AV_DP("PSB_AV_DP")
+}
+
+val GRUPPE_SOM_SKAL_SPESIALHÅNDTERES_NYE_REGLER = setOf(
+    Arbeidstype.KUN_YTELSE
+)
+
+val GRUPPE_SOM_SKAL_SPESIALHÅNDTERES_GAMLE_REGLER = setOf(
+    Arbeidstype.IKKE_YRKESAKTIV,
+    Arbeidstype.IKKE_YRKESAKTIV_UTEN_ERSTATNING,
+    Arbeidstype.KUN_YTELSE
+)
+
+val STANDARD_HÅNDTERING_NYE_REGLER = setOf(
+    Arbeidstype.ARBEIDSTAKER,
+    Arbeidstype.IKKE_YRKESAKTIV,
+    Arbeidstype.IKKE_YRKESAKTIV_UTEN_ERSTATNING,
+    Arbeidstype.FRILANSER,
+    Arbeidstype.SELVSTENDIG_NÆRINGSDRIVENDE
+)
+
+val STANDARD_HÅNDTERING_GAMLE_REGLER = setOf(
+    Arbeidstype.ARBEIDSTAKER,
+    Arbeidstype.FRILANSER,
+    Arbeidstype.SELVSTENDIG_NÆRINGSDRIVENDE
+)
+
+val YTELSER = setOf(
+    Arbeidstype.SYKEPENGER_AV_DAGPENGER,
+    Arbeidstype.PSB_AV_DP,
+    Arbeidstype.DAGPENGER,
+    Arbeidstype.INAKTIV
+)
+
+    fun getAktivitetsgruppe(skalBrukeNyeRegler: Boolean): List<Set<Arbeidstype>> {
+        if (FeatureToggle.isActive("IKKE_YRKESAKTIV_VEKTES_SOM_YRKESAKTIV")) {
+            if (skalBrukeNyeRegler) {
+                return listOf(
+                    STANDARD_HÅNDTERING_NYE_REGLER,
+                    YTELSER,
+                    GRUPPE_SOM_SKAL_SPESIALHÅNDTERES_NYE_REGLER
+                )
+            }
+        }
+        return listOf(
+            STANDARD_HÅNDTERING_GAMLE_REGLER,
+            YTELSER,
+            GRUPPE_SOM_SKAL_SPESIALHÅNDTERES_GAMLE_REGLER
+        )
+    }
+
+    fun getGruppeSomSkalSpesialhåndteres(skalBrukeNyeRegler: Boolean): Set<Arbeidstype> {
+        if (FeatureToggle.isActive("IKKE_YRKESAKTIV_VEKTES_SOM_YRKESAKTIV")) {
+            if (skalBrukeNyeRegler) {
+                return GRUPPE_SOM_SKAL_SPESIALHÅNDTERES_NYE_REGLER
+            }
+        }
+        return GRUPPE_SOM_SKAL_SPESIALHÅNDTERES_GAMLE_REGLER
+    }
