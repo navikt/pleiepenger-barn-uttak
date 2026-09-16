bff77b87db57b12195e6b374ef7f945482ae56fc
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2026-09-16 16:27:36 +0200

    Sett på @EnableTransactionManagement

--- server/src/main/kotlin/no/nav/pleiepengerbarn/uttak/server/db/DbConfig.kt
+++ server/src/main/kotlin/no/nav/pleiepengerbarn/uttak/server/db/DbConfig.kt
@@ -9,10 +9,12 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.transaction.annotation.EnableTransactionManagement
 import java.util.*
 import javax.sql.DataSource
 
 @Configuration
+@EnableTransactionManagement
 class DbConfig {
 
     companion object {
